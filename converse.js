1adb82371163dd7f2259fba934be18d8108ca9fc
Dom Steil <dominicsteil@hotmail.com>
2016-07-06 12:03:10 -0700

    adding conversation

---
+++ converse.js
@@ -0,0 +1,23 @@
+module.exports = {
+    helpMessage: "Here's what I can do:\n\n" +
+    "* Create new entities in Dynamics CRM like Quotes and Agreements",
+    canceled: 'Sure... No problem.',
+    saveQuoteCreated: "Created a new quote called '%(quote)s'",
+    listQuotes: 'Quote\n%s',
+    listNoQuotes: 'There are no quotes associated with that opportunity.',
+    deleteQuote: "Which quote would you like to delete?",
+    finishDeleteQuote: "Removed '%(task)s' from your task list.",
+    saveAgreementCreated: "Created a new agreement called '%(agreement)s'",
+    listAgreements: 'Agreement\n%s',
+    listNoAgreements: 'There are no agreements associated with that opportunity.',
+    deleteAgreement: "Which agreement would you like to delete?",
+    finishDeleteAgreement: "Removed '%(agreement)s' from your task list.",
+    saveAccountCreated: "Created a new Account called '%(Account)s'",
+    listAccounts: 'Account\n%s',
+    deleteQuote: "Which Account would you like to delete?",
+    finishDeleteQuote: "Removed '%(Account)s' from your task list.",
+    saveOpporunityCreated: "Created a new Account called '%(Account)s'",
+    listOpportunities: 'Opportunity\n%s',
+    deleteOpportunity: "Which Opportunity would you like to delete?",
+    finishDeleteOpportunity: "Removed '%(Opportunity)s' from your task list."
+}
