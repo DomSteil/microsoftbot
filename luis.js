20f7536f9b6be28956a48f7e872497bed9626587
Dom Steil <dominicsteil@hotmail.com>
2016-07-11 09:13:46 -0700

    updating botbuilder

--- luis.js
+++ luis.js
@@ -1,7 +1,13 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var converse = require('converse');
 
-var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=6bce4284-450b-47f7-acdc-003c95c92f4f&subscription-key=9ceaa7a6ab9d4f11851bd28c80521d71');
+var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=6bce4284-450b-47f7-acdc-003c95c92f4f&subscription-key=9ceaa7a6ab9d4f11851bd28c80521d71'
+
+var dialog = new builder.LuisDialog(model);
+model.exports = dialog;
+
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 bot.add('/', dialog);
 
 bot.add('/', new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=6bce4284-450b-47f7-acdc-003c95c92f4f&subscription-key=9ceaa7a6ab9d4f11851bd28c80521d71')
